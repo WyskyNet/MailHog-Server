c76efeaf958ea74459100a4dbf3c76ba05898a9f
WyskyNet <github@wysky.net>
2019-07-02 23:33:16 +0200

    Update v2.go

--- api/v2.go
+++ api/v2.go
@@ -111,7 +111,7 @@ func (apiv2 *APIv2) messages(w http.ResponseWriter, req *http.Request) {
 		panic(err)
 	}
 
-	var [] data
+	var data []
 	data = []data.Message(*messages)
 	sort.Slice(data, func(i, j int) bool{
 		return data[i].Created.After(data[j].Created)
